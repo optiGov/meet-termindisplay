b89f94ce80c0325a8f9413ba55d13ee67a7f56a9
PatrickGov <patrick@optigov.de>
2022-04-27 20:50:18 +0200

    add project config

--- src/main.js
+++ src/main.js
@@ -2,3 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+
+// global project config
+window.config = {
+    apiUrl: window.webpackHotUpdate ? "https://backend.optigov.de/core/api" : "/api",
+    oauth: {
+        endpoints: {
+            authorize: window.webpackHotUpdate ? "https://demo.optigov.de/oauth/authorize" : "/oauth/authorize",
+            token: window.webpackHotUpdate ? "https://demo.optigov.de/oauth/token" : "/oauth/token"
+        }
+    }
+}
